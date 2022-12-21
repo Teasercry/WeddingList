062834ed3d9b54e69d5b2182fa4b103669641883
Renan G. Silva <renan.amigos28@gmail.com>
2022-12-20 22:40:18 -0300

    feat: custom error 404

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Flex, VStack, Text } from "@chakra-ui/react";
 const App = () => {
   return(
     <Flex w="full" h="100vh" bgColor="blackAlpha.800" justifyContent="center" alignItems="center">
-        <Text color="white" fontWeight="bold" fontSize="2xl"> Error | 404</Text>
+        <Text color="white" fontWeight="thin" fontStyle="italic" fontSize="5xl"> Error | 404</Text>
     </Flex>
   );
 }
